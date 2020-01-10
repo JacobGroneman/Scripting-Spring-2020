94d3f6da24cae750cd5320e2c0ce1656e5837687
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-09 17:18:45 -0700

    Learning Data Persistence in Unity
    https://learn.unity.com/tutorial/persistence-saving-and-loading-data#

---
+++ Spring2020Project/Assets/Scripts/Data Persistence/AdjustScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustScript : MonoBehaviour
+{
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 100, 100, 30), "health Up"))
+        {
+            GameControl.control.health += 10;
+        }
+
+        if (GUI.Button(new Rect(10, 140, 100, 30), "health Down"))
+        {
+            GameControl.control.health -= 10;
+        }
+
+        if (GUI.Button(new Rect(10, 180, 100, 30), "Experience Up"))
+        {
+            GameControl.control.experience += 10;
+        }
+
+        if (GUI.Button(new Rect(10, 220, 100, 30), "Experience Down"))
+        {
+            GameControl.control.experience -= 10;
+        }
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/Data Persistence/GameControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl control;
+    
+    public float health;
+    public float experience;
+    
+    void Awake()
+    {
+        
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Health" + health);
+        GUI.Label(new Rect(10, 40, 150, 30), "Experience" + experience);
+    }
+}
