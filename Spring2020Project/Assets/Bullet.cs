a93fb2df617e778a6e12b1c8a2cc74b070c25ad5
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-10 23:05:58 -0700

    Did Brackeys Bullet Script Video
    https://www.youtube.com/watch?v=wkKsl1Mfp5M

---
+++ Spring2020Project/Assets/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class Bullet : MonoBehaviour
+{
+   public float speed = 20;
+   public Rigidbody2D rb;
+   
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        UnityEngine.Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+}
