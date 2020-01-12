d19ad43544b93155f21158e8f22d8cda2db3ab8a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-11 20:41:47 -0700

    Created a Unity Website advanced Tutorial Subscription Account

---
+++ Spring2020Project/Assets/Main.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnityEngine.Debug.Log("pressed the space bar.");
+        }
+        
+        if (Input.GetKey(KeyCode.E))
+        {
+            UnityEngine.Debug.Log("Holding E");
+        }
+
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            UnityEngine.Debug.Log("F got Pressed");
+        }
+    }
+}
---
+++ Spring2020Project/Assets/Position.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    public Vector3 startPosition;
+    
+    void Start()
+    {
+        transform.position = startPosition;
+    }
+}
