60150489f421bf38b09c2cf39e3f0a2f79bbc215
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-11 21:41:47 -0700

    More Unity Website Tutorial

---
+++ Spring2020Project/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    
+    [SerializeField]
+    private float _horizontalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _horizontalInput = Input.GetAxis("Horizontal");
+        
+        transform.Translate(new Vector3(_horizontalInput,0,0) * _speed * Time.deltaTime );
+    }
+}
