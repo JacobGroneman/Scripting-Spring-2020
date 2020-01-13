a85eaf53a97936cf07372fddcd091d39c17cfa02
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-12 17:09:48 -0700

    Learned Explosion Triggers
    https://learn.unity.com/project/c-survival-guide-rotations?courseId=5cf06bd1edbc2a58d7fc3209

---
+++ Spring2020Project/Assets/UnitySurvivalGuideScripts/Pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Time.timeScale = 0;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Time.timeScale = 0.25f;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 0.50f;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 0.75f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+}
