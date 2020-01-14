96028f91831d55434cecb2aa7a27531b528c4866
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-14 12:24:51 -0700

    Scripting Homework: Variables

---
+++ Spring2020Project/Assets/Scripts/ClassScripts/Assignments/Variables/VariablesHomework1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class VariablesHomework1 : MonoBehaviour
+{
+    public bool myBool = true;
+    public string galacticGreetings = "Hello Universe!";
+    public int interestingInt = 42;
+    public float sinkingFloat = -9.2f;
+    public Time timer = new Time();
+    public Quaternion quakerOats = Quaternion.identity;
+    public object iObject;
+    public char myLittleLetter = 'u';
+    public long howLong;
+    public double ohBabyATriple = 27.43;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
