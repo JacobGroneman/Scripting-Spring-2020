9065bfd32c4a08e7205e476af084e1e7ca0ba054
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-15 10:56:01 -0700

    Unity Survival Guide: Quaternion Look

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Rotations/Aim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _sphere;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       Vector3 directionToFace = _sphere.position - transform.position;
+       UnityEngine.Debug.DrawRay(transform.position, directionToFace, Color.magenta);
+       transform.rotation = Quaternion.LookRotation(directionToFace);
+    }
+}
