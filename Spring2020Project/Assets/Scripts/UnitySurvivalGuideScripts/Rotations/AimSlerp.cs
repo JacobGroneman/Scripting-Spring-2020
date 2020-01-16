61819d0994e31717fcff2bfdea12c852070e53f2
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-16 14:04:49 -0700

    Unity Survival Guide: Quaternion Slerps

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Rotations/AimSlerp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimSlerp : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _greenSphere;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionToFace = _greenSphere.position - transform.position;
+        UnityEngine.Debug.DrawRay(transform.position, directionToFace, Color.green);
+        Quaternion targetRotation = Quaternion.LookRotation(directionToFace);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+    }
+}
