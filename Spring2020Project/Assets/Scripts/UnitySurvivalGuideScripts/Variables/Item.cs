dc62eed5f5410238ef34672b079bc42dedd7867f
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-17 22:50:13 -0700

    Unity Survival Guide: Simple Variables Test

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Variables/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemSprite;
+    public float attack;
+    
+    void Start()
+    {
+        UnityEngine.Debug.Log("Item: " + itemName);
+        UnityEngine.Debug.Log("Description: " + itemDescription);
+        UnityEngine.Debug.Log("Sprite: " + itemSprite);
+        UnityEngine.Debug.Log("Attack: " + attack);
+    }
+    
+}
