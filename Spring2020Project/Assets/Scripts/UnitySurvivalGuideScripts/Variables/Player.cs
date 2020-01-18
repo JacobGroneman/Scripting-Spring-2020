39f4f3e7a3de15ed0f70cf69122a25757e775622
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-17 22:37:38 -0700

    Unity Survival Guide: Simple Variables Test

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Variables/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string name = "Jacob";
+    public int age = 21;
+    public float speed = 41.5f;
+    private float health = 93.2f;
+    private int score = 195;
+    private float speedStat = 41.5f;
+    private bool collected = false;
+    private int ammo = 0;
+    
+    void Start()
+    {
+        UnityEngine.Debug.Log("Name = " + name + "/ " + 
+                              "Age = " + age + "/ " + "Speed = " +
+                              speed + "/ "+ "Health = " + 
+                              health+ "/ " + "Score = " + score+ "/ "
+                              + "Speed Stat = " + speedStat+ "/ "
+                              + "Collected = " + collected+ "/ "
+                              + "Ammo = " + ammo+ "/ ");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
