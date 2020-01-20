cb16186cae20a03a2d3a877469650e5b72fd1526
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-20 01:09:00 -0700

    Unity Survival Guide: Finished If Statements

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/If Statements/IfQuizCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfQuizCalculator : MonoBehaviour
+{
+    public float 
+        quiz1, quiz2, quiz3, quiz4, quiz5;
+
+    public float average;
+
+    void Start()
+    {
+        quiz1 = Random.Range(1, 101);
+        quiz2 = Random.Range(1, 101);
+        quiz3 = Random.Range(1, 101);
+        quiz4 = Random.Range(1, 101);
+        quiz5 = Random.Range(1, 101);
+        
+        average = (quiz1 + quiz2 + quiz3 + quiz4 + quiz5)
+            /5;
+        
+        if (average > 90)
+        {
+            UnityEngine.Debug.Log
+            ("The " + 
+             "Grade average is: A with " + average + "%.");
+        }
+        else if (average >= 80 && average < 90)
+        {
+            UnityEngine.Debug.Log
+            ("The " + 
+             "Grade average is: B with " + average + "%.");
+        }
+        else if (average >= 70 && average < 80)
+        {
+            UnityEngine.Debug.Log
+            ("The " + 
+             "Grade average is: C with " + average + "%.");
+        }
+        else
+        {
+            UnityEngine.Debug.Log
+            ("The " + 
+             "Grade average is: F with " + average + "%.");
+        }
+    }
+
+   
+    void Update()
+    {
+       
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/If Statements/IfScorePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfScorePlayer : MonoBehaviour
+{
+    public GameObject cube;
+    public int score = 0;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            score += 10;
+        }
+
+        if (score >= 50)
+        {
+            cube.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+}
