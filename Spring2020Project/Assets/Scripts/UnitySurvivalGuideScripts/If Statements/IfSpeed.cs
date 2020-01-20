18d2e017748910fd85076a54c69b54ca7f54a11c
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-20 01:23:07 -0700

    Unity Survival Guide: Finished If Statements

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/If Statements/IfSpeed.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class IfSpeed : MonoBehaviour
+{
+    public int speed = 0;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            speed += 5;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            speed -= 5;
+        }
+
+        if (speed > 20)
+        {
+            UnityEngine.Debug.Log("Slow Down!");
+        }
+        else if (speed == 0)
+        {
+            UnityEngine.Debug.Log("Speed Up!");
+        }
+
+        if (speed < 0)
+        {
+            speed = 0;
+        }
+    }
+}
