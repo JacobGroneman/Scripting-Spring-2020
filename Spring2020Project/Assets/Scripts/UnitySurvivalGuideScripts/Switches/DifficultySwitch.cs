d1a390e1861daf6822917ee259bd5ca79cdf4b4c
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-21 11:38:01 -0700

    Unity Survival Guide: Switch Statement Overview

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Switches/DifficultySwitch.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class DifficultySwitch : MonoBehaviour
+{
+    public int difficultyLevel;
+    void Start()
+    {
+        difficultyLevel = Random.Range(0, 3);
+    }
+
+    
+    void Update()
+    {
+        switch (difficultyLevel)
+        {
+            case 0:
+                UnityEngine.Debug.Log("Easy");
+                break;
+            case 1:
+                UnityEngine.Debug.Log("Medium");
+                break;
+            case 2:
+                UnityEngine.Debug.Log("Hard");
+                break;    
+            default:
+                UnityEngine.Debug.Log("No Level");
+                break;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            difficultyLevel -= 1;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            difficultyLevel += 1;
+        }
+    }
+}
