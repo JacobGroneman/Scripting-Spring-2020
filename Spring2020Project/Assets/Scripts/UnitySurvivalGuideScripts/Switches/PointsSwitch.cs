be21cb3d1218374f9d8288ee0b90a23c9a46883d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-22 22:39:02 -0700

    Unity Survival Guide: Master Switch Statements 1

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Switches/PointsSwitch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointsSwitch : MonoBehaviour
+{
+    public int points;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            points = 50;
+        }
+        else if (Input.GetKeyDown(KeyCode.I))
+        {
+            points = 100;
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            points = 0;
+        }
+
+        switch (points)
+        {
+            case 50:
+                UnityEngine.Debug.Log("50 Points Yo");
+                break;
+            case 100:
+                UnityEngine.Debug.Log("100 Points Bro");
+                break;
+            default:
+                UnityEngine.Debug.Log("We only accept 50s or" +
+                                      "100s at this location Sir.");
+                break;
+        }
+    }
+
+
+}
