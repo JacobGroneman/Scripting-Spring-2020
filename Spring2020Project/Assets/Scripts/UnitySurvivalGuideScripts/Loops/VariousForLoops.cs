d95522b7e5a6741df09e7a7be5c6bcde30120051
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-24 22:57:01 -0700

    Made my own For Loops

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/VariousForLoops.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class VariousForLoops : MonoBehaviour
+{
+    
+    
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i % 2 == 0);
+            {
+                UnityEngine.Debug.Log("i is: " + i);
+            }
+        }
+
+        for (int u = 100; u > 100; u--)
+        {
+            if (u % 2 == 1);
+            {
+                UnityEngine.Debug.Log("U is: " + u);
+            }
+        }
+
+        for (int m = 100; m < 100; m++)
+        {
+            if (m == 5 || m == 27 || m == 47);
+            {
+                UnityEngine.Debug.Log("m is: " + m);
+            }
+        }
+        UnityEngine.Debug.Log("Loops are done y'all");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
