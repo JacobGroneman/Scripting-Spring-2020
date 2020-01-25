37870d877950f3596668060f424afccacac9ac13
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-24 22:38:38 -0700

    Unity Survival Guide: For Loop Overview

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/ForLoop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+
+    private string _theStereotypicalQuestion = "Are we there yet?" ;
+    
+    void Start()
+    {
+        for (int totalDistance = 0; totalDistance < 100; totalDistance++)
+        {
+            UnityEngine.Debug.Log(_theStereotypicalQuestion);
+        }
+        UnityEngine.Debug.Log("WE HAVE ARRIVED, NOW SHUT-UP!!");
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
