795b62d72ddcb0e31d0ad23cf3e2e75ed9f47323
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-25 20:56:27 -0700

    Unity Survival Guide: While Loops

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/WhileLoops.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoops : MonoBehaviour
+{
+
+    public int drops;
+
+
+    void Start()
+    {
+        while (drops < 100)
+        {
+            UnityEngine.Debug.Log("Drip");
+            drops++;
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator Money()
+    {
+        while (true)
+        {
+            UnityEngine.Debug.Log("It's true yo");
+            yield return new WaitForSeconds(1.00f);
+        }
+    }
+}
