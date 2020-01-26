48e6257222c9096aecac07d71e5d0929ba5f02a5
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-25 20:43:35 -0700

    Made My Own Do-While Loop in a CoRoutine

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/DoWhileLoop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class DoWhileLoop : MonoBehaviour
+{
+
+    public int gumballs = 100;
+    
+    void Start()
+    {
+        StartCoroutine(GumballDispenser());
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GumballDispenser()
+    {
+        do
+        {
+            UnityEngine.Debug.Log("There are " + gumballs + " gumballs.");
+            gumballs--;
+            yield return new WaitForSeconds(.5f);
+        } while (gumballs > 0);
+    }
+}
+
