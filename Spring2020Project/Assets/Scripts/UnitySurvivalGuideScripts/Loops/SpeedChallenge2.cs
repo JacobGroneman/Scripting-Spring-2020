cad1f0c3cc05ccc3cf9f393552c5c591c0fe224d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-27 10:59:53 -0700

    Made My Own Speed Loops

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/SpeedChallenge2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SpeedChallenge2 : MonoBehaviour
+{
+    public int speed;
+    public int maxSpeed;
+   
+    void Start()
+    {
+        StartCoroutine(speedCalculator());
+        maxSpeed = Random.Range(60, 120);
+    }
+
+
+    void Update()
+    {
+        
+    }
+    IEnumerator speedCalculator()
+    {
+        while (true)
+        {
+            speed += 5;
+            yield return new WaitForSeconds(.5f);
+            if (maxSpeed < speed)
+            {
+                break;
+            }
+        }
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/SpeedDoWhile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedDoWhile : MonoBehaviour
+{
+    public int speed, maxSpeed;
+
+    void Start()
+    {
+        maxSpeed = Random.Range(60, 120);
+        StartCoroutine(speedCalc());
+    }
+    
+    IEnumerator speedCalc()
+    {
+        do
+        {
+            speed += 5;
+            yield return new WaitForSeconds(1.0f);
+        } while (speed < maxSpeed);
+    }
+}
