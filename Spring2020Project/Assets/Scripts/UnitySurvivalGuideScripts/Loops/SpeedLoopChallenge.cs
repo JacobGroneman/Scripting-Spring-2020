9be66a51cd30ef8be93c99d46d7d4b84b29963e2
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-26 21:37:21 -0700

    Unity Survival Guide: Challenge Review: How Fast Are You Going?

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/SpeedLoopChallenge.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedLoopChallenge : MonoBehaviour
+{
+    public int speed;
+    public int maxSpeed;
+    
+    void Start()
+    {
+        maxSpeed = Random.Range(60, 120);
+        StartCoroutine(SpeedRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpeedRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            speed += 5;
+        }
+
+        if (speed > maxSpeed)
+        { 
+           yield break;
+        }
+    }
+}
