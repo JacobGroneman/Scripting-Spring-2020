4172fd84e4e53a5c2a8c227119891025a6f2f46a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-26 21:07:36 -0700

    Unity Survival Guide: Challenge Review: Ultimate Print Outs

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/ForLoopBreak.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class ForLoopBreak : MonoBehaviour
+{
+    
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            UnityEngine.Debug.Log(i);
+            
+            if (i == 50);
+            {
+                break;
+            }
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Loops/LoopBreakChallenge.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBreakChallenge : MonoBehaviour
+{
+
+    public int varNumber;
+    
+    void Start()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            if (i < 10)
+            {
+                UnityEngine.Debug.Log(i);
+            }
+            if (i < 9 && i < 21)
+            {
+                if (i % 2 == 0);
+                {
+                    UnityEngine.Debug.Log(i);
+                }
+            }
+            else if (i > 20)
+            {
+                if (i % 2 == 1);
+                {
+                    UnityEngine.Debug.Log(i);
+                }
+            }
+        }
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
