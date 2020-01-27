9f1f6b2c9aab3e93ae463c90367ca545d1bd564a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-27 11:29:41 -0700

    Unity Survival Guide: Challenge Review: Array Master

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Arrays/ArraySyntax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class ArraySyntax : MonoBehaviour
+{
+    public string[] names;
+    public int[] values = new int[6];
+    public float[] percentages = new float[]{5.3f, 2.2f, 35.6f, 45.2f};
+
+    public void Start()
+    {
+        UnityEngine.Debug.Log(names[2]);
+        UnityEngine.Debug.Log(values[3]);
+        UnityEngine.Debug.Log(percentages[2]);
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Arrays/ArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using UnityEngine;
+
+public class ArrayTest : MonoBehaviour
+{
+    public string[] names = new string[] {"Gill", "Bill", "Jill", "Phil", "Cecil"};
+    public int[] age = new int[]{5, 10, 15, 20, 37};
+    public string[] car = new string[]{"Bolt", "Dart", "Cruise", "Focus", "Fiesta"};
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnityEngine.Debug.Log(names[4] +" "+ age[4] +" "+ car[4]);
+        }
+    }
+}
