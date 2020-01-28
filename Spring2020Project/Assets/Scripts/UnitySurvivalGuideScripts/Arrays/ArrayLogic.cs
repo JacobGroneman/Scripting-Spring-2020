c2940962b66742dee10592b2de764a9897b6ccd8
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-28 15:26:38 -0700

    Unity Survival Guide: Print Out All Elements Using For Loop

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Arrays/ArrayLogic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayLogic : MonoBehaviour
+{
+    public int[] itemID = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public string[] items = new string[]{"shovel", "ax", "Sword", "Shield", "Mace", "Arrow", "Saber", "Wand", "ChinaStar", "Rock"};
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            for (int i = 0; i < itemID.Length; i++)
+            {
+                if (items[i] == "Sword");
+                {
+                    UnityEngine.Debug.Log("Item Found!");
+                    UnityEngine.Debug.Log("Number is: " + itemID[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Arrays/ArrayRandomTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class ArrayRandomTest : MonoBehaviour
+{
+    public int selectionNumber;
+    public string[] names = new string[] {"Gill", "Bill", "Jill", "Phil", "Cecil"};
+    public int[] age = new int[]{5, 10, 15, 20, 37};
+    public string[] car = new string[]{"Bolt", "Dart", "Cruise", "Focus", "Fiesta"};
+
+     void Start()
+     { 
+         
+     }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            selectionNumber = UnityEngine.Random.Range(0, names.Length);
+            UnityEngine.Debug.Log(selectionNumber);
+            UnityEngine.Debug.Log(names[selectionNumber] +" "+ age[selectionNumber] +" "+ car[selectionNumber]);
+        }
+        
+    }
+
+    
+}
