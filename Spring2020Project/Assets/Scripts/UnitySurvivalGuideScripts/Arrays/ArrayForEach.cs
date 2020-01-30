19656e6b918f64a21d6725cd4619f49367ca5502
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-29 23:08:13 -0700

    Unity Survival Guide: Print Out All Elements Using Foreach Loop

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Arrays/ArrayForEach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEngine;
+
+public class ArrayForEach : MonoBehaviour
+{
+    public string[] itemName = new string[]{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space));
+        {
+            foreach (var letter in itemName) 
+            {
+                UnityEngine.Debug.Log(letter);
+                
+                if (letter == "A");
+                {
+                    UnityEngine.Debug.Log(letter);
+                }
+            }
+        }
+    }
+}
