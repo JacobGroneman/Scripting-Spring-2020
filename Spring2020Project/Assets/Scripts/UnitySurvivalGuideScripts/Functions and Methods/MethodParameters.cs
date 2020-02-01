ad5f10b2d316b8f5e5e8b76753f44cf9325b80b7
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-01 11:32:21 -0700

    Unity Survival Guide: Method Parameters

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/MethodParameters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MethodParameters : MonoBehaviour
+{
+    public int health = 25;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q));
+        {
+            DamageAmount(5);
+        }
+    }
+    
+    
+    private void DamageAmount(int damage)
+    {
+        health =- damage;
+        
+        if (health < 1);
+        {
+            health = 0;
+            Destroy(this.gameObject);
+        }
+    }
+    
+}
