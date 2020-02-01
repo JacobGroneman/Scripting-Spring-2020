8d67cfa16f425053dbc8fb467eca9a72584fa874
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-01-31 21:41:10 -0700

    Unity Survival Guide: Loops Array Challenge

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Arrays/GameObjectArray.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectArray : MonoBehaviour
+{
+    
+    public GameObject[] objects;
+
+    void Update()
+    {
+        //For Each Loop
+        if (Input.GetKeyDown(KeyCode.Space));
+        {
+            foreach (var item in objects)
+            {
+                item.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }
+        //For Loop
+        /*if (Input.GetKeyDown(KeyCode.Space));
+        {
+            for (int i = 0; i < objects.Length; i++)
+            {
+                objects[i].GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+        }*/
+    }
+}
