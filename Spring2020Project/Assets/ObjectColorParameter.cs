5f7eea82ffc006233715c2b0a85f9bb5711787b2
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-02 20:44:36 -0700

    Unity Survival Guide: Parameter Functions &Passing the Object With Color

---
+++ Spring2020Project/Assets/ObjectColorParameter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColorParameter : MonoBehaviour
+{
+    public GameObject chameleon;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space));
+        {
+            ColorChange(chameleon, Color.red);
+        }
+    }
+
+    void ColorChange(GameObject obj, Color colorToAssign)
+    {
+        obj.GetComponent<MeshRenderer>().material.color = colorToAssign;
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/ReturnFunction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnFunction : MonoBehaviour
+{
+    public int total, total2,
+        total3;
+
+    void Start()
+    {
+        total = _Sum(5, 3);
+        total2 = _Sum(34, 57);
+        total3 = _Sum(28, 77) / _Product(32, 14);
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    private int _Sum(int a, int b)
+    {
+        return a + b;
+    }
+
+    private int _Product(int x, int y)
+    {
+        return x * y;
+    }
+}
