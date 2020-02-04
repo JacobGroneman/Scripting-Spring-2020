a082731855492cee24b133c5f796a3ebae558751
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-04 16:52:22 -0700

    Unity Survival Guide: Practical Presentation Return Array

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/PlayerColorReturn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColorReturn : MonoBehaviour
+{
+    public GameObject[] objectsToColor;
+    
+    void Start()
+    {
+        objectsToColor = ColorTheObjects();
+    }
+
+    GameObject[] ColorTheObjects()
+    {
+        GameObject[] colorArray = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (var obj in colorArray)
+        {
+            obj.GetComponent<MeshRenderer>().material.color = new Color(Random.value, Random.value, Random.value);
+        }
+        
+        return colorArray;
+    }
+}
