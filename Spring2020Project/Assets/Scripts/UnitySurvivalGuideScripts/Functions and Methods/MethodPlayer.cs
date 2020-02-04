a1e41789c0395a45cb08247e24c99f0038eb71b4
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-04 16:28:42 -0700

    Unity Survival Guide: Challenge: Are You Alive?

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/MethodPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MethodPlayer : MonoBehaviour
+{
+    public int health = 100;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsDead() == false)
+        {
+            DamageAmount(Random.Range(5, 25));
+        }
+    }
+
+    public void DamageAmount(int damage)
+    {
+        health -= damage;
+        
+        if (IsDead() == true)
+        {
+            UnityEngine.Debug.Log("The player has died!");
+            health = 0;
+        }
+    }
+     bool IsDead()
+    {
+        return health < 1;
+    }
+}
