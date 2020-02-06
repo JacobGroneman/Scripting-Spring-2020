db57f995ed2208a2b3cabf7f03a86823276f042d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-06 12:40:34 -0700

    Unity Survival Guide: Serialized Custom Class RPG Item Database Example

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Classes/Item1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Item1
+{
+   public string name;
+   public int itemID;
+   public string description;
+   public Sprite icon;
+   
+   public Item1(string name, int itemId, string description)
+   {
+      this.name = name;
+      this.itemID = itemId;
+      this.description = description;
+   }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Classes/ItemDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    private Item1 _sword, _hammer, _gun;
+    
+    void Start()
+    {
+        _sword = CreateItem("Sword", 0, "A Sword");
+        _hammer = CreateItem("Hammer", 01, "A Hammer");
+        _gun = CreateItem("Gun", 03, "Bing Bing");
+    }
+    
+    void Update()
+    {
+        
+    }
+    private Item1 CreateItem(string name, int itemID, string description)
+    { 
+        Item1 item = new Item1(name, itemID, description);
+        return item;
+    }
+}
