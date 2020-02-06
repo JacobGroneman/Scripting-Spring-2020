5cf17b6416e35add469c56ce19d7280cd1fd6d18
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-05 22:56:53 -0700

    Unity Survival Guide: Challenge Review: Position Matters

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/PositionArrayMethod.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionArrayMethod : MonoBehaviour
+{
+    public Vector3[] positions;
+    private int _randomIndex;
+    void Start()
+    {
+        _randomIndex = GetRandom();
+        UnityEngine.Debug.Log("Random Index: " + _randomIndex);
+        transform.position = GetPosition(_randomIndex);
+    }
+    private int GetRandom()
+    {
+        return Random.Range(0, positions.Length);
+    }
+
+    private Vector3 GetPosition(int index)
+    {
+        return positions[index];
+    }
+}
