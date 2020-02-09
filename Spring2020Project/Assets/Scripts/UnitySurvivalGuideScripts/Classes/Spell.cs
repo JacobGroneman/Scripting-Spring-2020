a169344ad124883e02089ba5d938c669fb372ec6
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-08 21:56:56 -0700

    Unity Survival Guide: RPG Spell System Presentation

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Classes/Spell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[System.Serializable]
+public class Spell
+{
+   public string spellName;
+   public int levelRequired;
+   public int itemIDRequired;
+   public int experienceGained;
+
+   public Spell(string name, int levelReq, int itemIDReq, int exGained)
+   {
+      this.spellName = name;
+      this.levelRequired = levelReq;
+      this.itemIDRequired = itemIDReq;
+      this.experienceGained = exGained;
+   }
+
+   public void CastSpell()
+   {
+      UnityEngine.Debug.Log("Spelled Casted: " + this.spellName);
+      
+   }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Classes/Wizard.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : MonoBehaviour
+{
+    public Spell fireBlast;
+    public int level;
+    public int ExpPoints;
+    
+    void Start()
+    { 
+        fireBlast = new Spell("Fire Blast", 2, 4, 2);   
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fireBlast.CastSpell();
+            ExpPoints += fireBlast.experienceGained;
+        }
+
+        if (ExpPoints >= 50)
+        {
+            level = 3; 
+        }
+    }
+}
