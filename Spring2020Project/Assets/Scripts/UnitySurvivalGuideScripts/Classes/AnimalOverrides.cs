91cd01ae42a464b67739b95b913417e4dbd40206
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-11 11:14:49 -0700

    Unity Survival Guide: Virtual Methods and Overriding

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Classes/AnimalOverrides.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalOverrides : MonoBehaviour
+{
+    protected string name;
+
+    protected virtual void Speak()
+    {
+        UnityEngine.Debug.Log("Speak");
+    }
+    void Start()
+    {
+        Speak();
+    }
+}
+
+public class Dog : AnimalOverrides
+{
+    protected override void Speak()
+    {
+        UnityEngine.Debug.Log("Bark!");
+    }
+}
+
+public class Duck : AnimalOverrides
+{
+    protected override void Speak()
+    {
+        UnityEngine.Debug.Log("Quack Y'all!");
+    }
+}
+
+public class PufferFish : AnimalOverrides
+{
+    protected override void Speak()
+    {
+        UnityEngine.Debug.Log("Glub Glub Lol");
+    }
+}
