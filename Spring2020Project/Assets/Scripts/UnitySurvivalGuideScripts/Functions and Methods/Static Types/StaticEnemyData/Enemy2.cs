bec8cfd62f338e6d6b4a3ac8c7db7fd8fe28e6e1
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-12 23:19:21 -0700

    Unity Survival Guide: Practical Example of Working with Static Types.

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/Static Types/StaticEnemyData/Enemy2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+
+public class Enemy2 : MonoBehaviour
+{
+   private UIManager _ui;
+   
+   private void OnEnable()
+   {
+      SpawnManager.EnemyCount++;
+      _ui = GameObject.Find("UserInterface").GetComponent<UIManager>();
+      _ui.UpdateEnemyCount();
+      Die();
+   }
+   private void OnDisable()
+   {
+      SpawnManager.EnemyCount--;
+   }
+
+   void Die()
+   {
+      Destroy(this.gameObject, UnityEngine.Random.Range(2, 6));
+      _ui.UpdateEnemyCount();
+   }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/Static Types/StaticEnemyData/SpawnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemy;
+    public static int EnemyCount;
+         void Start()
+         {
+             
+         }
+         
+         void Update()
+         {
+             if (Input.GetKeyDown(KeyCode.Space))
+             {
+                 Instantiate(enemy);
+             }
+         }
+}
