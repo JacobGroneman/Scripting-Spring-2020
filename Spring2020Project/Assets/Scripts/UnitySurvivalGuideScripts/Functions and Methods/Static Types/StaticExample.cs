c08dc916f2d4626cba0f589535e0f8ad4b5beeac
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-12 22:51:15 -0700

    Unity Survival Guide: Working with Static Types

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/Static Types/StaticExample.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticExample : MonoBehaviour
+{ 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Score._score += 10;
+        }
+    }
+    public class Score
+    {
+        public static int _score;
+    }
+}
