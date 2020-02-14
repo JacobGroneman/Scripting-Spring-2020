09b0ef75092e9bae2660c4c494978e722a7c55a8
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-13 22:11:54 -0700

    Unity Survival Guide: Challenge Review: Random Color Helper

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/Static Types/UtilityHelperClass/StaticColorChallenge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Color = System.Drawing.Color;
+
+public static class ChangeGameObject
+{
+    public static void RandomColor(GameObject obj)
+    {
+        obj.GetComponent<MeshRenderer>().material.color = new UnityEngine.Color
+            (Random.value, Random.value, Random.value);
+    }
+}
+
+public class StaticColorChallenge : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ChangeGameObject.RandomColor(gameObject);
+        }
+    }
+}
