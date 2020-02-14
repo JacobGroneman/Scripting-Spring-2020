d1d6be118fd280a971039c4c10094be79a5c8d68
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-13 18:33:23 -0700

    Instance Members vs Static Members

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/Static Types/StaticEnemyData/InstanceVsStatic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameThings
+{
+    public string itemName;
+    public int itemId;
+    public static int itemCount;
+
+    public GameThings(string itemName, int itemId)
+    {
+        this.itemName = itemName;
+        this.itemId = itemId;
+        itemCount++; //Static makes the function count all GameThing Instances.
+    }
+}
+
+public class InstanceVsStatic : MonoBehaviour
+{
+    void Start()
+    {
+        GameThings bread = new GameThings("Bread", 1);
+        GameThings waffle = new GameThings("Waffle", 02);
+        UnityEngine.Debug.Log("Item Count: " + GameThings.itemCount);
+    }
+}
