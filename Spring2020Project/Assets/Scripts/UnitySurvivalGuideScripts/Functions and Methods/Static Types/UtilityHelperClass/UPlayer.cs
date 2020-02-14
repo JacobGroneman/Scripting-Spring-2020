3bd9f27a57cbadb88304bebc4aecd987655f91ee
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-13 21:43:47 -0700

    Unity Survival Guide: Utility Helper Classes

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/Static Types/UtilityHelperClass/UPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UPlayer : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            Utility.CreateGameObject();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        { 
+            Utility.SetPositionZero(this.gameObject);
+        }
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Functions and Methods/Static Types/UtilityHelperClass/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utility
+{
+    public static void CreateGameObject()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+    public static void SetPositionZero(GameObject obj)
+    {
+      obj.transform.position = Vector3.zero;  
+    }
+}
