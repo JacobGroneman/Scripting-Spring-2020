455fcd4c3759212d3ac201270bc7af3dcf77c1c0
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-14 23:18:55 -0700

    Unity Survival Guide: Properties

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Properties/GetSetProperty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetSetProperty : MonoBehaviour
+{
+    private bool isGameOver;
+
+    public bool IsGameOver
+    {
+        get { return isGameOver; }
+        set { if (value == true) {UnityEngine.Debug.Log("The Game Is Over");} isGameOver = value;}
+    }
+
+    private void Start()
+    {
+        IsGameOver = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            IsGameOver = true;
+        }
+    }
+}
