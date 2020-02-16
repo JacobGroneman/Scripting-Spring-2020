c9a4b075b8c10f9928f42456ce7921b358228e16
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-15 21:50:28 -0700

    Unity Survival Guide: Challenge Review: Declaring Properties

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Properties/AutoProperty.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoProperty : MonoBehaviour
+{
+    public double BankAccount {get; protected set;}
+
+    void Start()
+    {
+        BankAccount = 5.72;
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Properties/ChallengeDeclareProperties.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class ChallengeDeclareProperties : MonoBehaviour
+{
+    private int Speed {get; set;}
+    public string name;
+
+    public string Name
+    {
+        get => name;
+        set
+        {
+            name = value;
+            UnityEngine.Debug.Log("Name Is Set");
+        }
+    }
+
+    void Start()
+    {
+        name = "Bill";
+        Speed = 3;
+    }
+}
