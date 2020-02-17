a426393d629bc4e6750459607abdf011dc1cb622
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-16 17:38:48 -0700

    Unity Survival Guide: Namespaces

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/NameSpaces/NameSpaceExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uvu10894805;
+
+namespace uvu10894805
+{
+    public class SampleClass
+    {
+        //Some Code
+    }
+}
+
+public class NameSpaceExample : MonoBehaviour
+{
+    void Start()
+    {
+        SampleClass test = new SampleClass();
+    }
+}
