1ddf3378b142904352f4e2fe4f3914bc8fdae03e
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-17 21:10:53 -0700

    Unity Survival Guide: Lists

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Lists/ListSyntax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListSyntax : MonoBehaviour
+{
+    public List<int> amounts;
+    public List<GameObject> stuff;
+    
+    void Start()
+    {
+        amounts.Capacity = 9;
+        amounts.Add( 6);
+
+        if (amounts.Contains(6))
+        {
+            amounts.Clear();
+        }
+        
+        stuff.Sort();
+       stuff.Add(gameObject);
+       stuff[0] = GameObject.Find("Player");
+
+    }
+}
