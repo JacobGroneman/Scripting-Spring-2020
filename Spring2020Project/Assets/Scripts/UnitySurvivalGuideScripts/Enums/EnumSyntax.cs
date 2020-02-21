a086abd3440c07bb9a57074e166fa83bc3152d7d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-20 22:27:19 -0700

    Unity Survival Guide: C# Enums

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Enums/EnumSyntax.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumSyntax : MonoBehaviour
+{
+   public enum LevelSelector
+   {
+       Easy,
+       Medium,
+       Hard,
+       Expert
+   }
+
+   public LevelSelector currentLevel;
+   
+    void Start()
+    {
+        switch (currentLevel)
+        {
+            case LevelSelector.Easy:
+                UnityEngine.Debug.Log("Current Level is Easy");
+                break;
+            case LevelSelector.Medium:
+                UnityEngine.Debug.Log("Current Level is Medium");
+                break;
+            case LevelSelector.Hard:
+                UnityEngine.Debug.Log("Current Level is Hard");
+                break;
+            case LevelSelector.Expert:
+                UnityEngine.Debug.Log("Current Level is Expert");
+                break;
+        }
+    }
+}
