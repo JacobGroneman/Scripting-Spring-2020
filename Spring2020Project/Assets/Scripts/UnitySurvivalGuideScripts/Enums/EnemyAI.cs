16d407e5bf9e30a35bf28cb7c103a8d9f8afc59d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-20 22:48:34 -0700

    Unity Survival Guide: C# Enums: Enemy AI

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Enums/EnemyAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+   public enum EnemyState
+   {
+       Patrolling,
+       Attacking,
+       Chasing,
+       Death
+   }
+
+   public EnemyState currentState;
+   
+   void Start()
+   {
+       currentState = EnemyState.Patrolling;
+   }
+   
+   void Update()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Patrolling:
+                UnityEngine.Debug.Log("Patrolling");
+                if (Time.time > 5) //very clever!
+                {
+                    currentState = EnemyState.Chasing;
+                }
+                break;
+            case  EnemyState.Attacking:
+                UnityEngine.Debug.Log("Attacking");
+                break;
+            case EnemyState.Chasing:
+                UnityEngine.Debug.Log("Chasing");
+                break;
+            case  EnemyState.Death:
+                UnityEngine.Debug.Log("Dying");
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentState = EnemyState.Death;
+        }
+    }
+}
