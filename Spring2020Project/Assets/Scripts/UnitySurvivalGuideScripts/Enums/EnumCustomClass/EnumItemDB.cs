9a4b3874c277d3a7092f1fe6a3c09b42f392ba50
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-21 00:58:00 -0700

    C# Enums: Custom Classes

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Enums/EnumCustomClass/EnumItemDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumItemDB : MonoBehaviour
+{
+    public List<EnumItem> itemDB = new List<EnumItem>();
+    
+    public void Update()
+    {
+        itemDB[0].Action();
+    }
+}
