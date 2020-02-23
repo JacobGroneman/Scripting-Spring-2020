23c9733b136bd102b4898c7f1e9f614898a03f0f
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-22 21:37:31 -0700

    Unity Survival Guide: C# Dictionary

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Dictionary/DictionarySyntax/DictItem.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DictItem
+{
+   public string name;
+   public int itemID;
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Dictionary/DictionarySyntax/DictionarySyntax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using UnityEngine;
+
+public class DictionarySyntax : MonoBehaviour
+{
+    public Dictionary<int, DictItem> itemDatabase = new Dictionary<int, DictItem>();
+    public DictItem 
+        sword, shield, gun, revolver, mace, chinaStar;
+
+    private void Start()
+    {//Find an easier way, like adding a List.
+        itemDatabase.Add(0, sword);
+        itemDatabase.Add(1, shield);
+        itemDatabase.Add(3, gun);
+        itemDatabase.Add(4, revolver);
+        itemDatabase.Add(5, mace);
+        itemDatabase.Add(6, chinaStar);
+
+        var item = itemDatabase[0];
+        item.name = "Sword";
+        item.itemID = 0;
+    }
+}
