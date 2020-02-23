eaf830e821723c732851751c80b9ef6006003181
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-22 21:14:06 -0700

    Unity Survival Guide: C# Enums: Casting Enums to Ints

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Enums/EnumCustomClass/LevelSelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelSelector : MonoBehaviour
+{
+    public enum DifficultyLevel
+    {
+        Easy, Medium, Hard, Expert
+    }
+
+    public DifficultyLevel levelIntensity;
+
+    private void Start()
+    {
+        SceneManager.LoadScene((int) levelIntensity);
+    }
+}
