71dfe5dfd7410c5ff06146e9f4e12c0f278ca63c
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-24 15:26:57 -0700

    Unity Survival Guide: C# Dictionary: Using Dict. for Player Connections

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Dictionary/DictPlayerConnections.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class OnlinePlayer
+{
+    public string playerName;
+    private int playerID {get;}
+
+    public OnlinePlayer (int playerID)
+    {
+        this.playerID = this.playerID;
+    }
+}
+
+public class DictPlayerConnections : MonoBehaviour
+{
+    public Dictionary<int, OnlinePlayer> onlinePlayerDictionary = new Dictionary<int, OnlinePlayer>();
+    
+    void Start()
+    {
+        var p1 = new OnlinePlayer(01) {playerName = "Player 1"};
+        var p2 = new OnlinePlayer(02) {playerName = "Player 2"};
+        var p3 = new OnlinePlayer(03) {playerName = "Player 3"};
+        onlinePlayerDictionary.Add(01, p1);
+        onlinePlayerDictionary.Add(02, p2);
+        onlinePlayerDictionary.Add(03, p3);
+
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var player3 = onlinePlayerDictionary[3];
+            UnityEngine.Debug.Log( player3.playerName + " Selected");
+        }
+    }
+}
