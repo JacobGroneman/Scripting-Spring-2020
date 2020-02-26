3577474e8b2de921ffcbdc393199936923ae4ec9
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-25 23:29:47 -0700

    Unity Survival Guide: C# Abstract Classes and Methods

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Abstract Classes/AbstractEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractEnemy : MonoBehaviour
+{
+    public int 
+        speed, gems;
+    public float 
+        health;
+
+    protected virtual void Die()
+    {
+        Destroy(this);
+    }
+
+    public abstract void Attack();
+}
+
+public class MossGiant : AbstractEnemy
+{
+    public override void Attack()
+    {
+        
+    }
+
+    protected override void Die()
+    {
+        base.Die();
+    }
+}
