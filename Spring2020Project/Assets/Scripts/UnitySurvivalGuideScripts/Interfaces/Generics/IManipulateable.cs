4c8e4309de7a3ef049565b6c355912876ce5d378
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-27 19:26:25 -0700

    Unity Survival Guide: Generic Interfaces

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Interfaces/Generics/IManipulateable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public interface IManipulateable<T>
+{
+    void VariableToManipulate(T variable);
+}
+
+public class GameObjectVar : MonoBehaviour, IManipulateable<GameObject>
+{
+    public void VariableToManipulate(GameObject variable)
+    {
+        variable = new GameObject();
+    }
+}
+
+public class IntVar : MonoBehaviour, IManipulateable<int>
+{
+    public void VariableToManipulate(int variable)
+    {
+        variable = 5;
+    }
+}
