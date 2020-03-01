cb69de7cb05ec2949ad4443eeb854444581ebdd3
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-29 21:11:14 -0700

    Unity Survival Guide: C# Events

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/EventsSyntax/EGameObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EGameObject : MonoBehaviour
+{
+    void Start()
+    {
+        EMain.onClick += TurnRed;
+    }
+
+    private void TurnRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    private void OnDisable()
+    {
+        EMain.onClick -= TurnRed;
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/EventsSyntax/EMain.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMain : MonoBehaviour
+{
+    public delegate void ActionClick();
+    public static event ActionClick onClick;
+    
+    public void ButtonClick()
+    {
+        onClick?.Invoke();
+    }
+}
