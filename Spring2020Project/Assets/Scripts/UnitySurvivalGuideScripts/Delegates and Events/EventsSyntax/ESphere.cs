c85e9a8da78b936829a5f8c91fbc430ad92d6661
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-02-29 22:38:21 -0700

    Unity Survival Guide: Working with C# Delegates and Events

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/EventsSyntax/ESphere.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESphere : MonoBehaviour
+{
+    void Start()
+    {
+        EMain.onClick += Fall;
+    }
+
+    private void Fall()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+}
