6e6b08460932048d8a0d2b0aa13b59cf2fcaef87
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-01 23:24:28 -0700

    Made My Own Color Delegate/Event System

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Practicing Events/EventCamera.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Color = System.Drawing.Color;
+using Random = System.Random;
+
+public class EventCamera : MonoBehaviour
+{
+   public delegate void ChangeColor(UnityEngine.Color color);
+   public static event ChangeColor onColorChange;
+
+   private void Update()
+   {
+      if(Input.GetKeyDown(KeyCode.K))
+      {
+         onColorChange?.Invoke(new UnityEngine.Color
+            (UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value));
+      }
+   }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Practicing Events/EventGameObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Color = System.Drawing.Color;
+
+public class EventGameObject : MonoBehaviour
+{
+    
+    void Start()
+    {
+        EventCamera.onColorChange += TurnColor;
+    }
+
+    private void OnDisable()
+    {
+        EventCamera.onColorChange -= TurnColor;
+    }
+
+    void TurnColor(UnityEngine.Color theColor)
+    {
+        this.GetComponent<MeshRenderer>().material.color = theColor;
+    }
+}
