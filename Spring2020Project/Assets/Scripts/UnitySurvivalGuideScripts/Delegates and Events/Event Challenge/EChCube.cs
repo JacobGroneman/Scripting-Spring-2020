3478ab21c253077c7069e7ab28201b0629d3d3b1
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-01 23:00:23 -0700

    Unity Survival Guide: Challenge: Teleport Events

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Event Challenge/EChCube.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EChCube : MonoBehaviour
+{
+    private void Start()
+    {
+        EChMain.onTeleport += ChangePosition;
+    }
+
+    private void OnDisable()
+    {
+        EChMain.onTeleport -= ChangePosition;
+    }
+
+    private void ChangePosition(Vector3 pos)
+    {
+        this.transform.position = pos;
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Event Challenge/EChMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EChMain : MonoBehaviour
+{
+   public delegate void Teleport(Vector3 pos);
+   public static event Teleport onTeleport;
+
+   private void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         onTeleport?.Invoke(new Vector3(5, 2, 0));
+      } 
+   }
+}
