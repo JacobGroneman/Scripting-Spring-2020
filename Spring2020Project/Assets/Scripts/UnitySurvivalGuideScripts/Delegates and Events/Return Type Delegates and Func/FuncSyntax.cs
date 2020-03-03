1c82e4cc5f3f2e45969416c9796eb817bf79884d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-02 21:34:40 -0700

    Unity Survival Guide: Return Type Delegates and Func

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Return Type Delegates and Func/FuncSyntax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class FuncSyntax : MonoBehaviour
+{
+    public Func<string, int> operateWord;
+    public string inputWord;
+
+    void Start()
+    {
+        operateWord = ReturnCharacterLength;
+        int count = ReturnCharacterLength(inputWord);
+        UnityEngine.Debug.Log("Number Length: " + count);
+    }
+    
+    int ReturnCharacterLength(string word)
+    {
+        return word.Length;
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Return Type Delegates and Func/ReturnDelegateSyntax.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ReturnDelegateSyntax : MonoBehaviour
+{
+    public delegate int WordOperations(string word);
+    public WordOperations operateWord;
+    public string wordOfChoice;
+   
+    void Start()
+    {
+        operateWord = ReturnCharacterLength;
+        UnityEngine.Debug.Log(operateWord);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            operateWord?.Invoke(wordOfChoice);
+        }
+    }
+
+    int ReturnCharacterLength(string word)
+    {
+        return word.Length;
+    }
+}
