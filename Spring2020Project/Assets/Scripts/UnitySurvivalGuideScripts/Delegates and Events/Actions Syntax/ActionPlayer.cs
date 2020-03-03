7d81f0897579668128e6cc7ec7148a16b7fa83c9
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-02 21:07:40 -0700

    Unity Survival Guide: C# Actions

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Actions Syntax/ActionPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionPlayer : MonoBehaviour
+{
+    public static Action<int> onDamageReceived;
+    public int Health {get; set;}
+
+    private void Start()
+    {
+        Health = 10;
+    }
+
+    void Damage()
+    {
+        onDamageReceived?.Invoke(Health);
+        Health--;
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Actions Syntax/ActionUIManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionUIManager : MonoBehaviour
+{
+   private void OnEnable()
+   {
+      ActionPlayer.onDamageReceived += UpdateHealthUI;
+   }
+
+   void UpdateHealthUI(int health)
+   {
+      UnityEngine.Debug.Log("Health: " + health);
+   }
+}
