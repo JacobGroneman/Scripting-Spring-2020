70af42d1f9fcea337b3e03873c20434730b8779a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-03 23:08:41 -0700

    Unity Engine: Practice Delegate of Type Void With Parameters
    I added on a Method to generate random #'s for the Action when the Space-bar is Pressed.

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/VoidLambdaAction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class VoidLambdaAction : MonoBehaviour
+{
+    public Action<int, int> Sum;
+
+    void Start()
+    {
+        Sum = (a, b) =>
+        {
+            var total = a + b;
+
+            UnityEngine.Debug.Log(total);
+
+            if (total < 100)
+            {
+                UnityEngine.Debug.Log("Total is less then 100");
+            }
+            else
+            {
+                UnityEngine.Debug.Log("Total is more then 100");
+            }
+        };
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RandomSum();
+        }
+    }
+
+    void RandomSum()
+    {
+        Sum?.Invoke(Random.Range(1, 100), Random.Range(1, 100));
+    }
+    
+}
