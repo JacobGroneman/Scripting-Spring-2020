ff58fb4140f5111b9c919a20041d466263cb72df
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-03 22:39:14 -0700

    Made My Own Lambda Expression

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/MyOwnLambda.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using Color = System.Drawing.Color;
+
+public class MyOwnLambda : MonoBehaviour
+{
+    private Func<MyOwnLambda, Vector3> RespawnPosition;
+
+    private void Start()
+    {
+        ActivateLambdas();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RespawnPosition?.Invoke(this);
+        }
+    }
+
+    private void ActivateLambdas()
+    {
+        RespawnPosition = (objectToRespawn) 
+            => objectToRespawn.GetComponent<Transform>().position = Vector3.zero;
+        
+    }
+}
