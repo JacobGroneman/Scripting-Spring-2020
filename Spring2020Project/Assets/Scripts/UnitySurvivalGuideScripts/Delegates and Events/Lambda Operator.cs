5be3f784ff87327d17cd75ad88f410b39d9d1066
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-03 22:21:55 -0700

    Unity Survival Guide: C# Lambda Expressions

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/Lambda Operator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class LambdaOperator : MonoBehaviour
+{
+    public Func<string, int> NumberCount;
+    
+    void Start()
+    {
+        NumberCount = (word) => word.Length;
+        int countResult = NumberCount("Boodgalooga");
+        UnityEngine.Debug.Log("Number of Letters: " + countResult);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
