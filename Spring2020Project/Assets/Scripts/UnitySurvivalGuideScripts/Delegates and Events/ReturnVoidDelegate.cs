a425f9c934cba5a56e6bd6d3202912ea76caf555
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-04 22:40:25 -0700

    Unity Survival Guide: Practice Return Delegate Without Parameters

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/ReturnVoidDelegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnVoidDelegate : MonoBehaviour
+{
+    public Func<int> onGetName;
+    
+   void Start()
+   {
+       onGetName = () => this.gameObject.name.Length;
+       var characterCount = onGetName?.Invoke();
+       Debug.Log("Name Length: " + characterCount);
+   }
+}
