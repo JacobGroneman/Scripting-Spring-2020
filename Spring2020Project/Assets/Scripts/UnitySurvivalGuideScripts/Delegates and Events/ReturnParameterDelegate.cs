486de2226ae99bb68f07ec240172107e77cf4f5b
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-04 22:52:13 -0700

    Unity Survival Guide: Practice Return Delegate and Parameters

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/ReturnParameterDelegate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class ReturnParameterDelegate : MonoBehaviour
+{
+    public Func<int, int, int> onCalculateSum;
+    void Start()
+    {
+        onCalculateSum = (a, b) => a + b;
+       var total = onCalculateSum?.Invoke(5, 42);
+       Debug.Log(name + "'s Total: " + total);
+    }
+}
