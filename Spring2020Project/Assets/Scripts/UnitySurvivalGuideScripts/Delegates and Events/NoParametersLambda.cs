167869fc2e8e6b3b4bd1a26f6b7cc0ec460f5905
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-04 22:21:34 -0700

    Unity Survival Guide: Practice Void Delegate With no Parameters using =>

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/NoParametersLambda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoParametersLambda : MonoBehaviour
+{
+    public Action onGetName;
+    void Start()
+    {
+        onGetName = () =>
+        {
+            Debug.Log("Game Object Name: " + name);
+            Debug.Log("It's a nice Day Y'all!");
+        };
+            
+        onGetName?.Invoke();
+    }
+}
