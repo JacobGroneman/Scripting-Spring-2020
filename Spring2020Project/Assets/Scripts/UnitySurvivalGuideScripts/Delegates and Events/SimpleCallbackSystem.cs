fd559053b5a3c544e8d2abcd3b5754dd4335e797
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-05 20:09:52 -0700

    Unity Survival Guide: Simple Callback System

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Delegates and Events/SimpleCallbackSystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class SimpleCallbackSystem : MonoBehaviour
+{
+    
+    void Start()
+    {
+        StartCoroutine(taskOne(() => Debug.Log("Task Complete!")));
+    }
+
+    private IEnumerator taskOne(Action onTaskComplete = null)
+    {
+       yield return new WaitForSeconds(5);
+       onTaskComplete?.Invoke();
+    }
+}
