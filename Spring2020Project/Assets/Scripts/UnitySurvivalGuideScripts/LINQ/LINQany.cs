8caabda0cba355fd4cf17685e9046469ee9b8933
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-05 20:29:29 -0700

    Unity Survival Guide: LINQ.Any

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/LINQ/LINQany.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LINQany : MonoBehaviour
+{
+    public string[] names = {"Agatha", "Belinda", "Claudell", "Dingus", "Emelita", "Fifo"};
+    void Start()
+    {
+        var nameFound = names.Any((name) => name == "Emelita");
+        Debug.Log("Name Found: " + nameFound);
+    }
+}
