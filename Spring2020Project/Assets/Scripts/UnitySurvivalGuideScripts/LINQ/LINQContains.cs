55e872fabb3677efae3e835b42a3f4d4a3350638
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-06 20:10:59 -0700

    Unity Survival Guide: LINQ.Contains
    Simple Simple, but applicable.

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/LINQ/LINQContains.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LINQContains : MonoBehaviour
+{
+    public string[] names = {"Agatha", "Belinda", "Claudell", "Dingus", "Emelita", "Fifo"};
+    void Start()
+    {
+        var nameFound = names.Contains("Agatha");
+        Debug.Log("Name Found: " + nameFound);
+    }
+}
