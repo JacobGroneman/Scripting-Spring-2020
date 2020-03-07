4b3b68c9dc5c8edcd39e2f085c6d47e477b32ed7
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-06 20:20:28 -0700

    Unity Survival Guide: LINQ.Distinct

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/LINQ/LINQDistinct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LINQDistinct : MonoBehaviour
+{
+    public string[] names =
+    {
+        "Kyle", "Dave", "Winston", "Paula",
+        "Ryan", "Dave", "Paula", "Dan", "John", "Ryan"
+    };
+
+    private void Start()
+    {
+        var uniqueNames = names.Distinct();
+        Debug.Log("Names: " + names);
+    }
+}
