1e09201f8d5ee466ec03ebc6342051ae514a900d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-07 23:42:04 -0600

    Unity Survival Guide: Order By Descending

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/LINQ/LINQOrderByDescending.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+public class LINQOrderByDescending : MonoBehaviour
+{
+    public int[] grades = 
+        {32, 85, 90, 97, 38, 65, 63, 87, 95, 92, 95, 84, 32, 45, 63, 69};
+
+    void Start()
+    {
+        var passingGrades = grades.Where((g) => g > 69).OrderByDescending((g) => g);
+        foreach (var grade in passingGrades)
+        {
+            Debug.Log(grade);
+        }
+    }
+}
