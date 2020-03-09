04df37fc1e9f81e1042f9e9117bcc94d7ea0eb38
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-08 21:56:34 -0600

    Unity Survival Guide: Singleton Design Pattern

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Singletons/SingletonSyntax/SingletonGameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonGameManager : MonoBehaviour
+{
+    private static SingletonGameManager _instance;
+
+    public static SingletonGameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("Error: The Game Manager is null.");
+            
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void DisplayName()
+    {
+        Debug.Log("My name is Schnazzle G.");
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Singletons/SingletonSyntax/SingletonPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonPlayer : MonoBehaviour
+{
+   private void Start()
+   {
+      SingletonGameManager.Instance.DisplayName();
+   }
+}
