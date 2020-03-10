3752420046604aaa792d6ad0afa2a991c244196d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-09 23:06:58 -0600

    Unity Survival Guide: Lazy Instantiation

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Singletons/Lazy Instantiation/LazyInstantiation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazyInstantiation : MonoBehaviour
+{
+  private static LazyInstantiation _instance;
+
+  public static LazyInstantiation Instance
+  {
+    get
+    {
+      if (_instance == null)
+      {
+        GameObject gameObj = new GameObject("Generic Manager"); // <--Lazy
+        gameObj.AddComponent<LazyInstantiation>();                    // Instantiation
+      }
+      return _instance;
+    }
+  }
+
+  private void Awake()
+  {
+    _instance = this;
+  }
+
+  public void GenericMethod()
+  {
+    Debug.Log("Stuff Happens");
+  }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Singletons/Lazy Instantiation/LazyPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazyPlayer : MonoBehaviour
+{
+    private void Start()
+    {
+        LazyInstantiation.Instance.GenericMethod();
+    }
+}
