852ee71965c1564a66dd04dac015abb0d3a00c6e
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-11 23:42:29 -0600

    Unity Survival Guide: Challenge: Pool Manager
    Object Pooling. I Need to review this again.

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Object Pooling/PoolPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolPlayer : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab);
+        }
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/Object Pooling/PoolPoolManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolPoolManager : MonoSingleton<PoolPoolManager>
+{
+    [SerializeField]
+    private GameObject _bullets;
+    [SerializeField]
+    private GameObject _bullet;
+    [SerializeField]
+    private List<GameObject> _bulletPool;
+
+    private void Start()
+    {
+        _bulletPool = GenerateBullets(10);
+    }
+
+    List<GameObject> GenerateBullets(int limit)
+    {
+        for (int i = 0; i < limit; i++)
+        {
+            GameObject bullet = Instantiate(_bullet);
+            _bullet.transform.parent = _bullets.transform;
+            _bullet.SetActive(false);
+            _bulletPool.Add(_bullet);
+        }
+        return _bulletPool;
+    }
+    
+
+}
