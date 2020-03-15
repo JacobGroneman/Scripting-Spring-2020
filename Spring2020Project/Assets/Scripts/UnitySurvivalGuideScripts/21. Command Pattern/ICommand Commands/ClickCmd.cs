68e8101ef3d3cb66102fa330e0e85bc9c91daab6
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-14 21:30:10 -0600

    Unity Survival Guide: Implementing the Command Pattern

---
+++ Spring2020Project/Assets/Scripts/UnitySurvivalGuideScripts/21. Command Pattern/ICommand Commands/ClickCmd.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickCmd : ICommand
+{
+    private GameObject _cube;
+    private Color _color, _previousColor;
+
+    public ClickCmd(GameObject cube, Color color)
+    {
+        this._cube = cube;
+        this._color = color;
+    }
+    
+    public void Execute()
+    {
+        _previousColor = _cube.GetComponent<MeshRenderer>().material.color
+            = _color;
+        _cube.GetComponent<MeshRenderer>().material.color
+            = _color;
+    }
+
+    public void Undo()
+    {
+        _cube.GetComponent<MeshRenderer>().material.color
+            = _previousColor;
+    }
+}
