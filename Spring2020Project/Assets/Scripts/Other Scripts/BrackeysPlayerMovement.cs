e04e2205e4b665a65c5002d06dd8ffaa3a5dbedb
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-18 22:09:45 -0600

    Brackey's 2D Movement in Unity (Tutorial)
    I rushed today from lack of time. . . But Happy Wasatch Wakeup Day Lol!!

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/BrackeysPlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BrackeysPlayerMovement : MonoBehaviour
+{
+    public CharacterController controller; 
+    [SerializeField] private float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    [SerializeField] private bool _jump = false;
+    [SerializeField] private bool _crouch = false;
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+       horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+       if (Input.GetButtonDown("Jump"));
+       {
+           _jump = true;
+       }
+       if (Input.GetButtonDown("Crouch"));
+       {
+           _crouch = true;
+       } 
+       else if (Input.GetButtonUp("Crouch"))
+       {
+           _crouch = false;
+       }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(new Vector3(horizontalMove * Time.fixedDeltaTime, _crouch, _jump));
+        _jump = false;
+    }
+}
