0e4d2c8f8a582deedc806d040c3723d5d9c9d20a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-19 23:11:27 -0600

    Text Adventure Game: Creating Rooms

--- Spring2020Project/Assets/Scripts/Unity Text Adventure Scripting/GameController.cs
+++ Spring2020Project/Assets/Scripts/Unity Text Adventure Scripting/GameController.cs
@@ -2,24 +2,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public Text displayText;
     [HideInInspector] public RoomNavigation roomNavigation;
-
+    List<string> actionLog = new List<string>();
+    
+//---------------------------------------------------------------------------------------------------------------------
     private void Awake()
     {
         roomNavigation = GetComponent<RoomNavigation>();
     }
 
-    void Update()
+    private void Start()
     {
-        
+        DisplayRoomText();
+        DisplayLogText();
+    }
+    
+    //-----------------------------------------------------------------------------------------------------------------
+    public void LogStringWithReturn(string stringToAdd)
+    {
+        actionLog.Add(stringToAdd + "\n");
     }
 
     public void DisplayRoomText()
     {
         string combinedText = roomNavigation.currentRoom.description + "\n";
+        LogStringWithReturn(combinedText);
+    }
+
+    public void DisplayLogText()
+    {
+        string logAsText = string.Join("\n", actionLog.ToArray());
+        displayText.text = logAsText;
     }
     
 }
