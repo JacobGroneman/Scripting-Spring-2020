5cb4486bfbe61efc79613636d925edc015f031fe
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-20 22:13:56 -0600

    Text Adventure Game: Text Input

---
+++ Spring2020Project/Assets/Scripts/Unity Text Adventure Scripting/Scriptable Objects/TextInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour
+{
+    public InputField inputField;
+    private GameController _gameController;
+    
+    private void Awake()
+    {
+        _gameController = GetComponent<GameController>();
+        inputField.onEndEdit.AddListener(AcceptStringInput);
+    }
+
+    void AcceptStringInput(string userInput)
+    {
+        userInput = userInput.ToLower();
+        _gameController.LogStringWithReturn(userInput);
+        InputComplete();
+    }
+
+    void InputComplete()
+    {
+        _gameController.DisplayLogText();
+        inputField.ActivateInputField();
+        inputField.text = null;
+    }
+}
