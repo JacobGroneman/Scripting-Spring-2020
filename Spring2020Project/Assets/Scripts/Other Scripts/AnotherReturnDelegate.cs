83a743b14ae642783ae449de023793c9bb91ec75
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-25 23:50:09 -0600

    Practiced Func Scripting

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/AnotherReturnDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using Debug = System.Diagnostics.Debug;
+
+public class AnotherReturnDelegate : MonoBehaviour
+{
+    public GameObject obj;
+    public Func<float, float, float, float> onCalculateAverageDistanceFromOrigin;
+    
+    void Start()
+    {
+        onCalculateAverageDistanceFromOrigin = (a, b, c) => a + b + c / 3;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            var position = obj.transform.position;
+            var average = onCalculateAverageDistanceFromOrigin?.Invoke(position.x, position.y, position.z);
+            UnityEngine.Debug.Log("Object's Position Average: " + average);
+        }
+    }
+}
