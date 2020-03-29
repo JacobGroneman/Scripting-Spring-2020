3a99670a085c165bfff5517d2c5cc778593d4296
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-28 21:42:46 -0600

    Text Adventure Game: Action Responses

---
+++ Spring2020Project/Assets/Scripts/Unity Text Adventure Scripting/Scriptable Objects/Action Response/ChangeRoomResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "TextAdventure/ActionResponse/ChangeRoom")]
+public class ChangeRoomResponse : ActionResponse
+{
+    public Room roomToChange;
+    
+    public override bool DoActionResponse(GameController gameController)
+    {
+        if (gameController.roomNavigation.currentRoom.roomName == requiredString)
+        {
+            gameController.roomNavigation.currentRoom = roomToChange;
+            gameController.DisplayRoomText();
+            return true;
+        }
+        return false;
+    }
+}
