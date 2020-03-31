0947ce5d8d67285249abbb37182e1f3acda63a03
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-30 23:21:12 -0600

    Brackey's First-Person Movement: Camera

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/First Person Movement/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    //Declarations
+    public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    public float xRotation = 0f;
+    
+    void Start()
+    {
+        //Cursor lock for Center Mouse Control
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //Declaring Mouse Input Directions
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        
+        //Down-Up Rotation Limit
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+        
+        //Vertical Rotation
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+        //Horizontal Rotation
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
