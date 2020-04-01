24ae79bb0f637c9ee7a2b1759f6f92e8b4c8d8c6
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-03-31 22:36:04 -0600

    Practiced Ray-casting for Distance Measuring

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/RayCastPractice.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastPractice : MonoBehaviour
+{
+    public float targetDistance;
+    
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit));
+        {
+            targetDistance = hit.distance;
+        }
+    }
+}
