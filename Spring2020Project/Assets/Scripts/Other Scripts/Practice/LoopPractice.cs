e169adf0f73a1d12115346f59244434cce1cd5ee
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-01 23:38:33 -0600

    Trying to Make Random Objects Change Colors Randomly Every Second

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/LoopPractice.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopPractice : MonoBehaviour
+{ 
+    void Start()
+    { 
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("ObjectsToColor");
+        
+        foreach (var obj in objs)
+        {
+            obj.transform.position = 
+                new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //ColorObjects();
+        }
+    }
+
+    /*IEnumerator ColorObjects()
+    {
+        for (int i = 0; i < objs.Length; i++)
+        {
+            colorObjs[Random.Range(0, colorObjs.Length)].
+                    GetComponent<MeshRenderer>().material.color 
+                = new Color(Random.value, Random.value, Random.value);
+        }
+        yield return new WaitForSeconds(1);
+    }*/
+}
+
