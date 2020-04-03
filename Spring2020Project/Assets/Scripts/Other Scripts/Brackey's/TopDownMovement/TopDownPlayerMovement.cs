4ccaaebdfadf95061bcbcf6bc46ca794c3518f18
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-02 21:53:53 -0600

    Brackeys: TOP DOWN MOVEMENT in Unity!

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/TopDownMovement/TopDownPlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownPlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+    
+    private Vector2 _movement;
+    
+    void Update()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+        
+        animator.SetFloat("Horizontal", _movement.x);
+        animator.SetFloat("Vertical", _movement.y);
+        animator.SetFloat("Speed", _movement.sqrMagnitude);
+        
+        //(for flipping)
+        //transform.localScale
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Time.fixedDeltaTime * moveSpeed * _movement);
+    }
+}
+
