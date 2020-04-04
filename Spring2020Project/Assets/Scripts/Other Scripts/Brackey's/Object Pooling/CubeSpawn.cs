a1d6ad0cda502b0d16bf9f0ee74f43c5d56f945a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-03 23:00:08 -0600

    Brackeys: OBJECT POOLING in Unity / CubeSpawner.cs CubeForces.cs

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/Object Pooling/CubeSpawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawn : MonoBehaviour
+{
+    public GameObject cubePrefabToSpawn;
+    
+    void FixedUpdate()
+    {
+        Instantiate(cubePrefabToSpawn, transform.position, Quaternion.identity);
+    }
+}
