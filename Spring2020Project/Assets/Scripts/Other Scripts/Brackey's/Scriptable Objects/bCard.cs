307023bace3cbb16d4d3f3da45d46bc36ceb3645
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-04 22:57:16 -0600

    Practiced Scriptable Object Instantiation
    I VERY glad this is easy for me now Lol

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/Scriptable Objects/bCard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Brackey's/Card", fileName = "New Card")]
+public class bCard : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite artwork;
+    public int mannaCost, attack, health;
+
+    public void PrintCardInfoToLog()
+    {
+        Debug.Log(name + ": " + description + " Card Cost: " + mannaCost);
+    }
+}
