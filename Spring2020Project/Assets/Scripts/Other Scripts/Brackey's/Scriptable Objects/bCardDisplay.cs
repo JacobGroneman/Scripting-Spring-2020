9c9eb123bfe4908516c59302ca5e30d07fc142f7
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-04 22:57:16 -0600

    Practiced Scriptable Object Instantiation
    I'm VERY glad this is easy for me now Lol

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/Scriptable Objects/bCardDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class bCardDisplay : MonoBehaviour
+{
+    public bCard card;
+
+    public Text 
+        nameUI, descriptionUI, manaCostUI, attackUI, healthUI;
+
+    public Image artUI;
+
+    private void Start()
+    {
+        card.PrintCardInfoToLog();
+        
+        nameUI.text = card.name;
+        descriptionUI.text = card.description;
+
+        manaCostUI.text = card.mannaCost.ToString();
+        attackUI.text = card.attack.ToString();
+        healthUI.text = card.health.ToString();
+        
+        card.artwork = artUI.sprite;
+    }
+}
