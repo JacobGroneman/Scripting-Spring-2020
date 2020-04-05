01be07af27a95af71c0d941435ac2fa609f32bc3
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-04 23:59:54 -0600

    Brackey's: SAVE & LOAD SYSTEM in Unity / Player.cs

--- Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/Save & Load Systems/bPlayer.cs
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/Save & Load Systems/bPlayer.cs
@@ -6,4 +6,22 @@ public class bPlayer : MonoBehaviour
 {
     public int level = 3;
     public float health = 89.5f;
+
+    public void SavePlayer()
+    {
+        bSaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        bPlayerData data = bSaveSystem.LoadPlayer();
+
+        level = data.level;
+        health = data.health;
+        Vector3 position;
+            position.x = data.position[0];
+            position.y = data.position[1];
+            position.z = data.position[2];
+                transform.position = position;
+    }
 }
