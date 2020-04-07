58f581d7544dd89dea45786564fe7d94860fe136
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-06 22:52:46 -0600

    Practiced Health Bar Systems and Expounded More Logic

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/Health Bar/Practice/Playerp.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerp : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth = 100;
+
+    public HealthBarp healthBar;
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        #region Inputs
+            
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            GetHealth(10);
+        }
+        
+        #endregion
+
+        #region MaxMinHealthLogic
+        
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+        }
+        else if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        
+        #endregion
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void GetHealth(int healthPoints)
+    {
+        currentHealth += healthPoints;
+        healthBar.SetHealth(currentHealth);
+    }
+}
