c77188bb1bd1c8bf03a385a89b27e9600baa34ad
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-06 23:08:55 -0600

    Practiced the Billboard Script for a World Health Bar

--- Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/Health Bar/Practice/BillBoardp.cs
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/Health Bar/Practice/BillBoardp.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BillBoardp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform cameraTransform;
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+        transform.LookAt(transform.position + cameraTransform.forward);
     }
 }
