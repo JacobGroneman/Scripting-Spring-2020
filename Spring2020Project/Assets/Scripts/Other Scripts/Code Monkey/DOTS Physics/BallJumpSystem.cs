c145fe277d540c613f6ae3a6af14930ccd735d3e
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-10 23:57:50 -0600

    Code Monkey: Getting Started with Unity DOTS Physics

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Code Monkey/DOTS Physics/BallJumpSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Physics;
+using UnityEngine;
+
+public class BallJumpSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref PhysicsVelocity physicsVelocity) =>
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                physicsVelocity.Linear.y = 5f;
+            }
+        });
+        
+    }
+}
