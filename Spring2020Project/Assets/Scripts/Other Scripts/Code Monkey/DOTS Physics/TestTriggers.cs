c69589f956bd48b9f6e6467777e345a848c967b3
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-12 21:20:50 -0600

    Code Monkey: Unity DOTS Trigger Method

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Code Monkey/DOTS Physics/TestTriggers.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using Unity.Mathematics;
+using Unity.Burst;
+
+[BurstCompile]
+public class TestTriggers : JobComponentSystem
+{
+    private struct TriggerJob : ITriggerEventsJob
+    {
+        public ComponentDataFromEntity<PhysicsVelocity> physicsVelocityEntities;
+        
+        public void Execute(TriggerEvent triggerEvent)
+        {
+            if (physicsVelocityEntities.HasComponent(triggerEvent.Entities.EntityA))
+            {
+                PhysicsVelocity physicsVelocity = physicsVelocityEntities[triggerEvent.Entities.EntityA];
+                physicsVelocity.Linear.y = 5f;
+                physicsVelocityEntities[triggerEvent.Entities.EntityA] = physicsVelocity;
+            }
+            
+            if (physicsVelocityEntities.HasComponent(triggerEvent.Entities.EntityB))
+            {
+                PhysicsVelocity physicsVelocity = physicsVelocityEntities[triggerEvent.Entities.EntityB];
+                physicsVelocity.Linear.y = 5f;
+                physicsVelocityEntities[triggerEvent.Entities.EntityA] = physicsVelocity;
+            }
+        }
+    }
+
+    private BuildPhysicsWorld _buildPhysicsWorld;
+    private StepPhysicsWorld _stepPhysicsWorld;
+    
+    protected override void OnCreate()
+    {
+        _buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        _stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        TriggerJob triggerJob = new TriggerJob
+        {
+            physicsVelocityEntities = GetComponentDataFromEntity<PhysicsVelocity>()
+        };
+        return triggerJob.Schedule(_stepPhysicsWorld.Simulation, ref _buildPhysicsWorld.PhysicsWorld, inputDeps);
+    }
+}
