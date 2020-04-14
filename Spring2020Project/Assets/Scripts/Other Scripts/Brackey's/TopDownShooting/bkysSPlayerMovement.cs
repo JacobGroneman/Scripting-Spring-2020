a8a1ece369a34999cf8407ef535039ebc94205d0
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-13 22:39:57 -0600

    Brackeys: TOP DOWN SHOOTING in Unity: Player Vector2 Movement w/Rotation

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Brackey's/TopDownShooting/bkysSPlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class bkysSPlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    
+    public Rigidbody2D rb;
+    
+    public Vector2 movement, mousePosition;
+    
+    public Camera cam;
+    
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 directionToLook = mousePosition - rb.position;
+        
+        //Research Atan and Atan2
+        float angle = Mathf.Atan2(directionToLook.y, directionToLook.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
