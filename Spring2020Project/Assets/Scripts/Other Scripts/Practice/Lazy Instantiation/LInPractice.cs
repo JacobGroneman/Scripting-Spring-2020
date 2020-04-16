8a88fa54508f4ea1f38a07e704efd5319c5b4ada
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-15 23:24:44 -0600

    Practiced Lazy Instantiation

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/Lazy Instantiation/LInPractice.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LInPractice : MonoBehaviour
+{
+    private static LInPractice _instance;
+
+    public static LInPractice Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject gameObj = new GameObject("Manager");
+                gameObj.AddComponent<LInPractice>();
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void Method()
+    {
+        Debug.Log("Executing Method");
+    }
+}
