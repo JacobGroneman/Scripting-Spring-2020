ed78b726cee1596110a85bd68418596d08e0c3bd
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-15 23:57:03 -0600

    Practiced Return Parameter Delegation with Object Destruction

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/ReturnDelegatePractice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class ReturnDelegatePractice : MonoBehaviour
+{
+    public GameObject gO1, gO2;
+
+    public Func<GameObject, GameObject, float> onCalculateAverageScale;
+    void Start()
+    {
+        onCalculateAverageScale = (a, b) => (
+            (a.transform.lossyScale.x * a.transform.lossyScale.y * a.transform.lossyScale.z) +
+            (b.transform.lossyScale.x * b.transform.lossyScale.y * b.transform.lossyScale.z) /2 );
+        
+       var total = onCalculateAverageScale?.Invoke(gO1, gO2);
+       
+       Debug.Log("The scale average of " + gO1.name + " and " + gO2.name + " is: " + total);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DestroySmallestGameObject();
+        }
+    }
+
+    void DestroySmallestGameObject()
+    {
+        float gO1Volume = gO1.transform.lossyScale.x * gO1.transform.lossyScale.y * gO1.transform.lossyScale.z;
+        float gO2Volume = gO2.transform.lossyScale.x * gO2.transform.lossyScale.y * gO2.transform.lossyScale.z;
+        
+        if (gO1Volume >= gO2Volume)
+        {
+            Destroy(gO2);
+        }
+        else if (gO1Volume <= gO2Volume)
+        {
+            Destroy(gO1);
+        }
+    }
+}
