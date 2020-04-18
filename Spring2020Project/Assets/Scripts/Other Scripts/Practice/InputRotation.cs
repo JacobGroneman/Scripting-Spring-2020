6ec486ec831d9fa9b29c73eeede093ae563b11c3
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-17 23:30:59 -0600

    Practiced Game Object Rotation Based on Input (It's Incorrect (Buggy))

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/InputRotation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputRotation : MonoBehaviour
+{
+    public GameObject cube;
+    public Quaternion rotation;
+
+    void Update()
+    {
+        cube.transform.rotation = rotation;
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            rotation.x++;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rotation.x--;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rotation.y++;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rotation.y--;
+        }
+    }
+}
