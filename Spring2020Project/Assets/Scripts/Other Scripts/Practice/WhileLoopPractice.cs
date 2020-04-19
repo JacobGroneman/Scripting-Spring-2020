fb89d4f979d7726da2b45b085d0983b9d4d32ec4
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-18 23:08:14 -0600

    Practiced Making a Countdown Timer

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/WhileLoopPractice.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoopPractice : MonoBehaviour
+{
+   public int seconds = 0;
+   private bool _pressed = false;
+
+   void Update()
+   {
+      if (_pressed == false)
+      {
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+            StartCoroutine(RunTimer());
+            _pressed = true;
+         }
+      }
+   }
+
+   private IEnumerator RunTimer()
+   {
+      while (true)
+      {
+         yield return new WaitForSeconds(1);
+         seconds--;
+      }
+   }
+}
