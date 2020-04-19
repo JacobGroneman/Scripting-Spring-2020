b75004342e3c65149969ad08b2e2088e034274f2
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-18 23:26:15 -0600

    Practiced Making a Count-Up Timer

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/TimerDisplayPractice.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerDisplayPractice : MonoBehaviour
+{
+    private int _days, _hours, _minutes, _seconds;
+    
+    public Text timerUI;
+    
+    private string _timerText = "0:0:0:0";
+    private bool _isTimerStarted = false;
+
+    void Start()
+    {
+        timerUI.text = _timerText;
+    }
+
+    void Update()
+    {
+        if (_isTimerStarted == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine(StartTimer());
+                _isTimerStarted = true;
+            }
+        }
+    }
+
+    IEnumerator StartTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            _seconds++;
+            
+            if (_seconds == 60)
+            {
+                _seconds = 0;
+                _minutes++;
+            }
+
+            if (_minutes == 60)
+            {
+                _minutes = 0;
+                _hours++;
+            }
+
+            if (_hours == 24)
+            {
+                _hours = 0;
+                _days++;
+            }
+
+            timerUI.text = 
+                _days.ToString() + ":" + _hours.ToString() + 
+                ":" + _minutes.ToString() + ":" + _seconds.ToString();
+            Debug.Log(
+                _days.ToString() + ":" + _hours.ToString() + 
+                ":" + _minutes.ToString() + ":" + _seconds.ToString());
+        }
+    }
+}
