e3bd5e70fb2b202860cc8239f0af415f2f6da967
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-19 21:00:52 -0600

    Blackthornprod - COOL DIALOG SYSTEM

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/DialogPractice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogPractice : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    public float typingSpeed;
+    
+    private int _index;
+
+    IEnumerator Typer()
+    {
+        foreach (char letter in sentences[_index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        
+    }
+}
