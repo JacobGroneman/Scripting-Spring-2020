5b5838d76102eabf45ec0e4e82dd26cb51791b32
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-19 21:31:43 -0600

    Blackthornprod - COOL DIALOG SYSTEM pt.2

--- Spring2020Project/Assets/Scripts/Other Scripts/Practice/DialogPractice.cs
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/DialogPractice.cs
@@ -8,8 +8,27 @@ public class DialogPractice : MonoBehaviour
     public TextMeshProUGUI textDisplay;
     public string[] sentences;
     public float typingSpeed;
+
+    public GameObject continueButton;
+    public Animator textDisplayAnim;
+    private AudioSource _source;
     
     private int _index;
+
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+        StartCoroutine(Typer());
+
+    }
+    
+    void Update()
+    {
+        if (textDisplay.text == sentences[_index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
 
     IEnumerator Typer()
     {
@@ -22,6 +41,21 @@ public class DialogPractice : MonoBehaviour
 
     public void NextSentence()
     {
+        _source.Play();
+        textDisplayAnim.SetTrigger("Change");
+        continueButton.SetActive(false);
+        
+        if (_index < sentences.Length - 1)
+        {
+            _index++;
+            textDisplay.text = " ";
+            StartCoroutine(Typer());
+        }
+        else
+        {
+            textDisplay.text = " ";
+            continueButton.SetActive(false);
+        }
         
     }
 }
