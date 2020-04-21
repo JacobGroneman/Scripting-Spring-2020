079f83f72dc3802d9aa68d37e86a3bd43faf9a52
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-20 22:32:09 -0600

    Unity Learn AI: Vectors Part 1

---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/Vectors/AIMove.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMove : MonoBehaviour
+{
+    private Vector3 _goal = new Vector3(5, 0, 4);
+    
+    void Update()
+    {
+        this.transform.Translate(_goal);
+    }
+}
