48eec3d876955f3cf4cbe5f0e775443e797739c3
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-21 20:43:56 -0600

    Unity Learn AI: Vectors Part 2

---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/Vectors/AIMoveToGoal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMoveToGoal : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float goalAccuracy = 0.01f;
+    public Transform goal;
+    
+    void LateUpdate()
+    {
+        this.transform.LookAt(goal.position);
+        
+        Vector3 direction = goal.position - this.transform.position;
+
+        Debug.DrawRay(this.transform.position, direction, Color.magenta);
+
+        if (direction.magnitude > goalAccuracy)
+        {
+            this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
