319a4b86279664a16edc5e8cabc8203bab77e967
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-23 23:15:12 -0600

    Practiced Making a Color Get/Set Script (Doesn't Work, Needs Fix)

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/ColorGetSet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorGetSet : MonoBehaviour
+{
+    public GameObject obj;
+
+    public bool IsObjAffected
+    {
+        get => IsObjAffected;
+        set
+        {
+            if (value == true)
+            {
+                Debug.Log(obj.name + " is affected.");
+                obj.GetComponent<MeshRenderer>().material.color = Color.green;
+            }
+        }
+    }
+    
+    void Start()
+    {
+        obj.GetComponent<MeshRenderer>().material.color = Color.white;
+        IsObjAffected = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (IsObjAffected == false)
+            {
+                IsObjAffected = true;
+            }
+        }
+    }
+}
