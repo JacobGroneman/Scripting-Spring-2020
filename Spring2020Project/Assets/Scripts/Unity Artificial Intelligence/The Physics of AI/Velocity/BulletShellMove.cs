95340bfd2bf03e23fc5dc48964a5cadfb537741a
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-29 22:32:08 -0600

    Unity Learn AI: Velocity
    (Simple)

---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/The Physics of AI/Velocity/BulletShellMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShellMove : MonoBehaviour
+{
+    private float _speed;
+    
+    void Update()
+    { 
+        //(Time.DeltaTime = 1 meter/second)
+        
+        //this.transform.Translate(0, 0,  _speed * Time.deltaTime);
+        
+        //this.transform.Translate(0, _speed * Time.deltaTime, _speed * Time.deltaTime);
+        
+        var realTime = _speed * Time.deltaTime;
+        
+        this.transform.Translate(realTime, realTime, realTime);
+    }
+}
