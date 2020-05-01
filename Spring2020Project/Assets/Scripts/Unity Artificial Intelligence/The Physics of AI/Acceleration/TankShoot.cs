c54a14d90eb6ab51e976471508ebad8a3b78d411
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-04-30 21:40:25 -0600

    Unity Learn AI: Acceleration

---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/The Physics of AI/Acceleration/TankShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankShoot : MonoBehaviour
+{
+    public GameObject shellPrefab, shellSpawnPosition;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        GameObject shell =
+            Instantiate(shellPrefab, shellSpawnPosition.transform.position, 
+                shellSpawnPosition.transform.rotation);
+    }
+}
