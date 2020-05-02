eece7caf2da3f29057879181ef624253abf799ac
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-05-01 18:33:19 -0600

    Practicing Item Collection

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/ItemCollection/ItemCollectionPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollectionPlayer : MonoBehaviour
+{
+    public List<GameObject> collectibles;
+    private int _points;
+    
+    void Start()
+    {
+        PlayerUIManager.Instance.points.text = _points.ToString();
+        PlayerUIManager.Instance.itemCount.text = collectibles.Count.ToString();
+    }
+    
+    void OnColliderEnter(Collider col)
+    {
+        if (col.gameObject.tag == "point")
+        {
+            _points++;
+            PlayerUIManager.Instance.points.text = _points.ToString();
+        }
+
+        if (col.gameObject.tag == "Collectible")
+        {
+           
+           PlayerUIManager.Instance.itemCount.text = collectibles.Count.ToString();
+        }
+    }
+
+    void LateUpdate()
+    {
+        
+    }
+}
