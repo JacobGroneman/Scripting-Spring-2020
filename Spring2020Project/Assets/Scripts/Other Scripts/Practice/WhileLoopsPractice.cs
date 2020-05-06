aa96c274a4dd4202d1823ce1a13a5e0c90e2e076
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-05-05 23:52:52 -0600

    Had an Emergency and Tried to Fudge a Script

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/WhileLoopsPractice.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoopsPractice : MonoBehaviour
+{
+
+    public int gumDrops;
+
+
+    void Start()
+    {
+        while (gumDrops < 100)
+        {
+            UnityEngine.Debug.Log("Eating Gumdrops");
+            gumDrops++;
+        }
+    }
+    
+    IEnumerator Money()
+    {
+        while (true)
+        {
+            UnityEngine.Debug.Log("It's true yo");
+            yield return new WaitForSeconds(1.00f);
+        }
+    }
+}
---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/The Physics of AI/Waypoints & Graphs/Astar for Pathfinding/WaypointManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Link
+{
+    public enum direction {UNI, BI};
+    public GameObject node1, node2;
+    public direction dir;
+}
+
+public class WaypointManager : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public Link[] links;
+    public Graph graph = new Graph();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
