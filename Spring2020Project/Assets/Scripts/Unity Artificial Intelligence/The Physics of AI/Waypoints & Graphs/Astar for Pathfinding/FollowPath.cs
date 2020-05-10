73a996fef899f14102a4e3611025727fcb87ad0c
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-05-09 21:57:01 -0600

    Unity Learn AI: Astar for Pathfinding Part 1

---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/The Physics of AI/Waypoints & Graphs/Astar for Pathfinding/FollowPath.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    private Transform _goal;
+    private float _velocity = 5.0f;
+    private float _accuracy = 1.0f;
+    private float _rotationalVelocity = 2.0f;
+    public GameObject waypointManager;
+    private GameObject[] _waypoints;
+    private GameObject _currentNode;
+    private int currentWaypoint = 0;
+    private Graph _graph;
+
+    void Start()
+    {
+        _waypoints = waypointManager.GetComponent<WaypointManager>().waypoints;
+        _graph = waypointManager.GetComponent<WaypointManager>().graph;
+        _currentNode = _waypoints[0];
+    }
+
+    void LateUpdate()
+    {
+        if (_graph.getPathLength() == 0 || currentWaypoint == _graph.getPathLength())
+            return;
+
+        //Closest Current WayPoint
+        _currentNode = _graph.getPathPoint(currentWaypoint);
+
+        //If close to Current WayPoint, Move to Next WayPoint
+        if (Vector3.Distance(
+                _graph.getPathPoint(currentWaypoint).transform.position, 
+                this.transform.position) < _accuracy)
+        {
+            currentWaypoint++;
+        }
+        
+        //If Path Isn't Completed
+        if (currentWaypoint < _graph.getPathLength())
+        {
+            _goal = _graph.getPathPoint(currentWaypoint).transform;
+            Vector3 _lookAtGoal = new Vector3(
+                _goal.position.x, this.transform.position.y, _goal.position.z);
+            Vector3 _direction = _lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                Quaternion.LookRotation(_direction), Time.deltaTime * _rotationalVelocity);
+        }
+    }
+
+    public void GoToHeliPad()
+    {
+        _graph.AStar(_currentNode, _waypoints[4]);
+        currentWaypoint = 0;
+    }
+
+    public void GoToRuin()
+    {
+        _graph.AStar(_currentNode, _waypoints[0]);
+        currentWaypoint = 0;
+    }
+}
--- Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/The Physics of AI/Waypoints & Graphs/Astar for Pathfinding/WaypointManager.cs
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/The Physics of AI/Waypoints & Graphs/Astar for Pathfinding/WaypointManager.cs
@@ -14,15 +14,28 @@ public class WaypointManager : MonoBehaviour
 {
     public GameObject[] waypoints;
     public Link[] links;
-    //public Graph graph = new Graph();
+    public Graph graph = new Graph();
 
     void Start()
     {
-        
+        if (waypoints.Length > 0)
+        {
+            foreach (var waypoint in waypoints)
+            {
+                graph.AddNode(waypoint);
+            }
+
+            foreach (var link in links)
+            {
+                graph.AddEdge(link.node1, link.node2);
+                if (link.dir == Link.direction.BI)
+                    graph.AddEdge(link.node2, link.node1);
+            }
+        }
     }
 
     void Update()
     {
-        
+        graph.debugDraw();
     }
 }
