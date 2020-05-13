7afe0c70e98e634c85ea9312f8e6c90fda7b4f32
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-05-12 21:24:09 -0600

    Unity Learn AI: AI Player Follow and Off-Mesh Links

---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/Nav Meshes/Player Follow & Off-Mesh Links/ChasePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    private GameObject _player;
+    private NavMeshAgent _nmAg;
+
+    void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+        _nmAg = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        _nmAg.SetDestination(_player.transform.position);
+    }
+}
