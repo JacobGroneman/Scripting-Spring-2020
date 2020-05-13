ffcab5cbfab60e49812720fdcb876d83a228b049
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-05-12 21:46:02 -0600

    Unity Learn AI: Finite State Machines

---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/Finite State Machines/Finite State Machines/GameEnvironment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment : MonoBehaviour
+{
+    private static GameEnvironment instance;
+    private List<GameObject> _checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints {get { return _checkpoints;}}
+
+    public static GameEnvironment Singleton
+    {
+        get
+        {
+            if ( instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return instance;
+        }
+    }
+}
