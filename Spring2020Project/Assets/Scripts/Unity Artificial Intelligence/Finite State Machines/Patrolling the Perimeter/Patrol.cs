52965ceee529be2e143f1e1010604935982ebe48
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-05-14 21:52:39 -0600

    Unity Learn AI: Patrolling the Perimeter (1)

---
+++ Spring2020Project/Assets/Scripts/Unity Artificial Intelligence/Finite State Machines/Patrolling the Perimeter/Patrol.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol : StateClass
+{
+    private int _currentIndex = -1;
+    public Patrol
+        (GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player) 
+        : base(_npc, _agent, _anim, _player)
+    {
+        name = STATE.PATROL;
+        agent.speed = 2;
+        agent.isStopped = false;
+    }
+
+    public override void Enter()
+    {
+        _currentIndex = 0;
+        anim.SetTrigger("isWalking");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        if (agent.remainingDistance < 1)
+        {
+            if (_currentIndex >= GameEnvironment.Singleton.Checkpoints.Count - 1) 
+                _currentIndex = 0;
+            else
+                _currentIndex++;
+            
+            agent.SetDestination
+                (GameEnvironment.Singleton
+                .Checkpoints[_currentIndex].transform.position);
+        }
+        base.Update();
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isWalking");
+        base.Exit();
+    }
+    
+}
