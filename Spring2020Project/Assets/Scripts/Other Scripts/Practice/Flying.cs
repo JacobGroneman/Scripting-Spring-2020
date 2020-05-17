dfcd8df8f51f994d202e1f3dae176be2ae9c5703
Jacob Groneman <jacobjazzdrummer@gmail.com>
2020-05-16 23:04:55 -0600

    Practiced State Creation Inherited From StateClass

---
+++ Spring2020Project/Assets/Scripts/Other Scripts/Practice/Flying.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Flying : StateClass
+{
+    public Flying
+        (GameObject _npc, NavMeshAgent _agent, Animator _anim, Transform _player)
+        : base(_npc, _agent, _anim, _player)
+    {
+        //name = STATE.FLYING;
+        agent.speed = 10.0f;
+        agent.isStopped = false;
+    }
+
+    public override void Enter()
+    {
+        anim.SetTrigger("isFlying");
+        base.Enter();
+    }
+
+    public override void Update()
+    {
+        agent.acceleration = 02.0f;
+        
+        Vector3 flyingDestination = Vector3.up;
+        agent.SetDestination(flyingDestination);
+
+        if (!CanSeePlayer())
+        {
+            //nextState = new Fall(npc, agent, anim, player);
+            stage = EVENT.EXIT;
+        }
+    }
+
+    public override void Exit()
+    {
+        anim.ResetTrigger("isFlying");
+        base.Exit();
+    }
+}
+
